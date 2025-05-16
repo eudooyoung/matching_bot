e7b47584b9ea5e3dba710dd1425dd2d014c9fa0a
eudooyoung <vuiettlain@gmail.com>
2025-05-16 09:31:22 +0900

    first commit

---
+++ src/main/java/com/multi/matchingbot/MatchingBotApplication.java
@@ -0,0 +1,13 @@
+package com.multi.matchingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingBotApplication.class, args);
+    }
+
+}
