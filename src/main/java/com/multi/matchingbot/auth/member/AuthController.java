fa7e03b32139ea4f2c97ff98914e89d5eedf1264
DESKTOP-DE7V0VP\com2023 <cocytus106@naver.com>
2025-05-19 15:38:34 +0900

    Auth

--- src/main/java/com/multi/matchingbot/auth/member/AuthController.java
+++ src/main/java/com/multi/matchingbot/auth/member/AuthController.java
@@ -1,15 +1,15 @@
-package com.multi.matchingbot.auth;
-
-import com.multi.matchingbot.auth.member.MemberDto;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-@PostMapping("/signup")
-public ResponseEntity<MemberDto> signup(@RequestBody MemberDto memberDto) {
-    MemberDto saved = AuthService.signup(memberDto);
-    return ResponseEntity
-            .status(HttpStatus.CREATED)
-            .body(new MemberDto(HttpStatus.CREATED, "회원가입 성공", saved));
-}
+//package com.multi.matchingbot.auth;
+//
+//import com.multi.matchingbot.auth.member.MemberDto;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//@PostMapping("/signup")
+//public ResponseEntity<MemberDto> signup(@RequestBody MemberDto memberDto) {
+//    MemberDto saved = AuthService.signup(memberDto);
+//    return ResponseEntity
+//            .status(HttpStatus.CREATED)
+//            .body(new MemberDto(HttpStatus.CREATED, "회원가입 성공", saved));
+//}
--- src/main/java/com/multi/matchingbot/auth/member/MemberMapper.java
+++ src/main/java/com/multi/matchingbot/auth/member/MemberMapper.java
@@ -1,11 +1,11 @@
-package com.multi.matchingbot.auth.member;
-
-import java.util.Optional;
-
-@Mapper
-public interface MemberMapper {
-
-    int insertMember(MemberDto memberDto);
-
-    Optional<MemberDto> findByEmail(String email);
-}
+//package com.multi.matchingbot.auth.member;
+//
+//import java.util.Optional;
+//
+//@Mapper
+//public interface MemberMapper {
+//
+//    int insertMember(MemberDto memberDto);
+//
+//    Optional<MemberDto> findByEmail(String email);
+//}
