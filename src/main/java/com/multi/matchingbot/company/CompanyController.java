85fd0076cfb3a05728b83aff39119e2f293daeed
Han Se Bin <sebinhan4@gmail.com>
2025-05-21 09:39:10 +0900

    기업 마이페이지

--- src/main/java/com/multi/matchingbot/company/CompanyController.java
+++ src/main/java/com/multi/matchingbot/company/CompanyController.java
@@ -1,6 +1,5 @@
 package com.multi.matchingbot.company;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +11,6 @@ public class CompanyController {
 
     @GetMapping("/")
     public String showCompanyHome(Model model) {
-        Long companyId = 1L;
-
-        model.addAttribute("company_name", companyDto.getName());
-
         return "/company/index"; // templates/index.html 렌더링
     }
 }
