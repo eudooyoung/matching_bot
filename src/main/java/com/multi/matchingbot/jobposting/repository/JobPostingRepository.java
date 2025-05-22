36aa1d01d1886e456ac6b5ae8548bbcaae8ddf67
Han Se Bin <sebinhan4@gmail.com>
2025-05-22 11:15:14 +0900

    sebin 커밋 3

--- src/main/java/com/multi/matchingbot/jobposting/repository/JobPostingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multi.matchingbot.jobposting.repository;
-
-import com.multi.matchingbot.jobposting.domain.JobPosting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobPostingRepository extends JpaRepository<JobPosting, Long> {
-}
