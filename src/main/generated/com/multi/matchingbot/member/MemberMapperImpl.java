987cefd4cdbb87abb0ef31c83052131d959b008a
DESKTOP-DE7V0VP\com2023 <cocytus106@naver.com>
2025-05-27 10:06:41 +0900

    비밀번호 재설정 5.27

--- src/main/generated/com/multi/matchingbot/member/MemberMapperImpl.java
+++ src/main/generated/com/multi/matchingbot/member/MemberMapperImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-25T20:47:34+0900",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+    date = "2025-05-27T09:45:41+0900",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.14 (Azul Systems, Inc.)"
 )
 @Component
 public class MemberMapperImpl implements MemberMapper {
---
+++ src/main/java/com/multi/matchingbot/auth/find/dto/FindUserPasswordDto.java
@@ -0,0 +1,18 @@
+package com.multi.matchingbot.auth.find.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FindUserPasswordDto {
+    private String email;
+    private String name;
+    private String phone1;
+    private String phone2;
+    private String phone3;
+
+    public String getFullPhone() {
+        return phone1 + "-" + phone2 + "-" + phone3;
+    }
+}
