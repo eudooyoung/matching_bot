ba4f89a32caef2bb77465c3ddc7b8312c49b8d7b
eudooyoung <vuiettlain@gmail.com>
2025-06-02 17:29:38 +0900

    pushing swagger

--- src/main/java/com/multi/matchingbot/attachedItem/util/ReportImageGenerator.java
+++ src/main/java/com/multi/matchingbot/attachedItem/util/ReportImageGenerator.java
@@ -60,4 +60,43 @@ public class ReportImageGenerator {
         log.info("리포트 이미지 생성 성공 (w={}px, h={}px)", image.getWidth(), image.getHeight());
         return image;
     }
+
+    public BufferedImage convertSummaryToImage(Map<String, Object> reportData) throws Exception {
+        // html 렌더링
+        Context context = new Context();
+        context.setVariables(reportData);
+        String html = templateEngine.process("chatbot/evaluation-report", context);
+
+        // html -> pdf
+        ByteArrayOutputStream pdfOutPut = new ByteArrayOutputStream();
+        PdfRendererBuilder builder = new PdfRendererBuilder();
+        File fontFile = new File(getClass().getClassLoader().getResource("fonts/NanumGothic.ttf").toURI());
+        System.out.println("✅ 폰트 파일 존재? " + fontFile.exists());
+        System.out.println("✅ 경로: " + fontFile.getAbsolutePath());
+        builder.useFont(fontFile, "NanumGothic");
+        builder.useFastMode();
+        builder.withHtmlContent(html, null);
+        builder.toStream(pdfOutPut);
+        builder.run();
+
+        // pdf -> image
+        BufferedImage image;
+        try (PDDocument document = PDDocument.load(new ByteArrayInputStream(pdfOutPut.toByteArray()))) {
+            PDFRenderer renderer = new PDFRenderer(document);
+            BufferedImage fullImage = renderer.renderImageWithDPI(0, 200);  // 화질 제어
+            log.info("PDF 페이지 높이: {}px", fullImage.getHeight());
+            log.info("PDF 페이지 너비: {}px", fullImage.getWidth());
+
+            // ✅ 위쪽, 아래쪽 잘라내기
+            int cropTop = 60;
+            int cropBottom = 100;
+            int croppedHeight = fullImage.getHeight() - cropTop - cropBottom;
+
+            image = fullImage.getSubimage(0, cropTop, fullImage.getWidth(), croppedHeight);
+            //            image = fullImage;
+        }
+
+        log.info("리포트 이미지 생성 성공 (w={}px, h={}px)", image.getWidth(), image.getHeight());
+        return image;
+    }
 }
