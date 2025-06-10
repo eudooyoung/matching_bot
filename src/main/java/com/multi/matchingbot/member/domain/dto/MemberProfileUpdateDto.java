18164689fb666efdff9839aefe0e70992c1401c3
DESKTOP-DE7V0VP\com2023 <cocytus106@naver.com>
2025-06-10 16:15:48 +0900

    구직자 기능 수정

--- src/main/java/com/multi/matchingbot/member/domain/dto/MemberProfileUpdateDto.java
+++ src/main/java/com/multi/matchingbot/member/domain/dto/MemberProfileUpdateDto.java
@@ -1,6 +1,10 @@
 package com.multi.matchingbot.member.domain.dto;
 
 import com.multi.matchingbot.common.domain.enums.Gender;
+import com.multi.matchingbot.common.domain.enums.Yn;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
@@ -23,7 +27,8 @@ import java.time.LocalDate;
 public class MemberProfileUpdateDto {
 
     private Long id;
-
+    private Boolean agreeLocation;
+    private Boolean agreeMarketing;
     private String email; // 읽기 전용
 
     @NotBlank(message = "이름은 필수입니다.")
@@ -31,10 +36,12 @@ public class MemberProfileUpdateDto {
     private String name;
 
     @Pattern(regexp = "^[가-힣a-zA-Z0-9_-]{2,15}$", message = "닉네임은 2~15자의 한글, 영문, 숫자, '_', '-'만 사용할 수 있습니다.")
+    @Column(unique = true, length = 15)
     private String nickname;
 
     @NotBlank(message = "주소는 필수입니다.")
     private String address;
+    private String addressDetail;
 
     @NotBlank(message = "전화번호는 필수입니다.")
     @Pattern(regexp = "^(010|011|016|017|018|019)-?\\d{3,4}-?\\d{4}$", message = "유효한 휴대폰 번호 형식이어야 합니다.")
@@ -47,4 +54,6 @@ public class MemberProfileUpdateDto {
     @PastOrPresent(message = "생년월일은 현재 날짜 이전이어야 합니다.")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birth;
+
+
 }
