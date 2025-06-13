12906852bd4c1811222ab09a8794990a4972891b
eudooyoung <vuiettlain@gmail.com>
2025-06-13 21:28:38 +0900

    dooyoung: comment modified in authserviceimpl

--- src/main/java/com/multi/matchingbot/auth/service/AuthenticationServiceImpl.java
+++ src/main/java/com/multi/matchingbot/auth/service/AuthenticationServiceImpl.java
@@ -126,14 +126,14 @@ public class AuthenticationServiceImpl implements AuthenticationService {
         // 쿠키 생성
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", accessToken)
                 .httpOnly(true)
-                .secure(false) // TODO: 배포 시 true로 전환
+                .secure(false) // TODO: https 배포 시 true로 전환
                 .path("/")
                 .maxAge(tokenProvider.getAccessTokenExpireTime())
                 .build();
 
         ResponseCookie refreshCookie = ResponseCookie.from("refreshToken", refreshToken)
                 .httpOnly(true)
-                .secure(false)  // TODO: 배포 시 true로 전환
+                .secure(false)  // TODO: https 배포 시 true로 전환
                 .path("/")
                 .maxAge(tokenProvider.getRefreshTokenExpireTime())
                 .build();
@@ -181,7 +181,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         ResponseCookie accessCookie = ResponseCookie.from("accessToken", newAccessToken)
                 .httpOnly(true)
-                .secure(false) // TODO:!!!!배포시 true로 전환!!!!!
+                .secure(false) // TODO: https 배포시 true로 전환
                 .path("/")
                 .maxAge(tokenProvider.getAccessTokenExpireTime())
                 .build();
